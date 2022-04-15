1bbac2b20740e492a5cc2320b3f88581658cc272
rudinkaca <rudin.kaca@crispybacon.al>
2022-04-15 09:44:11 +0200

    [client]: Dashboard check if user exist

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../features/auth/authSlice';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -9,6 +9,11 @@ const Dashboard = () => {
 
   const { user } = useSelector((state) => state.auth);
 
+  useEffect(() => {
+    if(!user) {
+      navigate('/login');
+    }
+  }, [user, navigate])
   const onLogout = () => {
     dispatch(logout());
     dispatch(reset());
